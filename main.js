0a9f61764529b887e974fe542b8afe5192712a25
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-01-19 16:48:45 -0500

    commit tree 5bf405b8aff7cdd85390df299456fd28cced2601

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector('#modal');
+const message = document.querySelector('#model-message');
 
+let hearts = document.querySelectorAll(".like") 
+
+for (let heart of hearts) {
+  heart.addEventListener("click", like)
+}
+
+function like(e) {
+  if (e.target.innerText === EMPTY_HEART) {
+    mimicServerCall()
+    .then(() => {
+      e.target.innerText = FULL_HEART;
+      e.target.classList.toggle('activated-heart');
+
+    })
+    .catch(err => {
+      modal.classList.toggle('hidden')
+      message.innerText = err;
+      setTimeout(() => modal.classList.toggle('hidden'), 5000);
+    })
+  }
+    if (e.target.innerText === FULL_HEART) {
+      e.target.innerText = EMPTY_HEART;
+      e.target.classList.toggle('activated-heart')
+    }
+}
 
 
 
